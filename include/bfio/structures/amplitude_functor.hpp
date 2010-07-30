edb2246e165fbe2a64598256465cfe9d48395a49
Jack Poulson <jack.poulson@gmail.com>
2010-07-30 12:16:59 -0500

    Added missing amplitude functor header file.

---
+++ include/bfio/structures/amplitude_functor.hpp
@@ -0,0 +1,39 @@
+/*
+  Copyright 2010 Jack Poulson
+
+  This file is part of ButterflyFIO.
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 3 of the License, or 
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BFIO_AMPLITUDE_FUNCTOR_HPP
+#define BFIO_AMPLITUDE_FUNCTOR_HPP 1
+
+#include "bfio/structures/data.hpp"
+
+namespace bfio {
+
+// You will need to derive from this class and override the operator()
+template<typename R,unsigned d>
+class AmplitudeFunctor
+{
+public:
+    virtual ~AmplitudeFunctor() {}
+    virtual std::complex<R> operator() 
+    ( const Array<R,d>& x, const Array<R,d>& p ) const = 0;
+};
+
+} // bfio
+
+#endif /* BFIO_AMPLITUDE_FUNCTOR_HPP */
+
