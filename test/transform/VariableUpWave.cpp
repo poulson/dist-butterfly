66bb60da9f1a517a04f244a1cf1659c7e3ad7fdc
Jack Poulson <jack.poulson@gmail.com>
2010-09-16 20:36:22 -0500

    Removed '-fast' flag from default build and modified drivers to include <algorithm>.

--- test/transform/VariableUpWave.cpp
+++ test/transform/VariableUpWave.cpp
@@ -15,6 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <algorithm>
 #include <ctime>
 #include <fstream>
 #include <memory>
